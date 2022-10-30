9ad66cabf38938774c4ac6185a08a7b5603badd1
kimyechan <vlvkcjswo7@gmail.com>
2022-10-30 23:13:19 +0900

    문자열을 다루는 법

---
+++ src/main/kotlin/_21문자열을다루는법/Example.kt
@@ -0,0 +1,35 @@
+package _21문자열을다루는법
+
+fun main() {
+    val test1 = "Test.Kotlin.String"
+
+    println(test1.length)
+
+    println(test1.lowercase())
+    println(test1.uppercase())
+
+    val test2 = test1.split(".")
+    println(test2)
+
+    println(test2.joinToString())
+    println(test2.joinToString("-"))
+
+    println(test1.substring(5..10))
+
+    val nullString: String? = null
+    val emptyString = ""
+    val blankString = " "
+    val normalString = "A"
+
+    println(nullString.isNullOrEmpty())
+    println(emptyString.isNullOrEmpty())
+    println(blankString.isNullOrEmpty())
+    println(normalString.isNullOrEmpty())
+
+    println()
+
+    println(nullString.isNullOrBlank())
+    println(emptyString.isNullOrBlank())
+    println(blankString.isNullOrBlank())
+    println(normalString.isNullOrBlank())
+}
